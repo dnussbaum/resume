ff1ad4669e870f3d6d518ccf43fe66f03a498ad8
Daniel Nussbaum <daniel.e.nussbaum@gmail.com>
2016-07-26 22:30:14 -0700

    initial commit

---
+++ build.sh
@@ -0,0 +1,10 @@
+rm Daniel\ Nussbaum\ Resume.pdf
+rm daniel-nussbaum-resume.pdf
+cd src
+xelatex resume.tex resume.pdf
+rm resume.out
+rm resume.log
+rm resume.aux
+mv resume.pdf ../Daniel\ Nussbaum\ Resume.pdf
+cd ..
+cp Daniel\ Nussbaum\ Resume.pdf daniel-nussbaum-resume.pdf
